dd8d057beca5c77bd1741e49cdfd509d0e16091b
v1c7or20 <victorpa793@gmail.com>
2019-09-30 23:58:29 -0500

    Done

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
-#include <stack>
 #include <string>
-#include "Node.h"
+#include "Three.h"
+
 
 using namespace std;
 
 int main(){
     string postfixed = topost();
+    stack<string> done;
     int place= 0;
-    cout<<postfixed;
-
+    cout<<postfixed<<endl;
+    makevector(done,postfixed);
+    Node* arbol = new Node(done);
+    cout<<arbol->execute(arbol->maps)<<endl;
     return 0;
 }
 
